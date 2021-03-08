e1ddeff850567fb490c6e1cc8e1dcf8b1ef4ca3d
Jurgen Segaert <jsegaert@yahoo.com>
2021-03-07 17:53:21 -0700

    Switch to media user, for consistency with deluge-pip

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # iocage-plugin-deluge
 Artifact file(s) for Deluge iocage plugin
+
+This plugin will install Deluge and its WebUI from the FreeBSD package
+
+## To install this Plugin
+Download the plugin manifest file to your local file system.
+```
+fetch https://raw.githubusercontent.com/jsegaert/iocage-my-plugins/master/deluge.json
+```
+Install the plugin.  Adjust the network settings as needed.
+```
+iocage fetch -P deluge.json -n deluge
+```
+
--- post_install.sh
+++ post_install.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
-sysrc -f /etc/rc.conf deluged_enable="YES"
-sysrc -f /etc/rc.conf deluged_user="nobody"
-sysrc -f /etc/rc.conf deluged_confdir="/usr/local/etc/deluge"
-sysrc -f /etc/rc.conf deluge_web_enable="YES"
-sysrc -f /etc/rc.conf deluge_web_user="nobody"
-sysrc -f /etc/rc.conf deluge_web_confdir="/usr/local/etc/deluge"
-mkdir -p /usr/local/etc/deluge
-chown -R nobody:nobody /usr/local/etc/deluge
 
-echo "Deluge Web UI Password: deluge" > /root/PLUGIN_INFO
+# Prepare the system
+pw useradd -n deluge -u 8675309 -m -c "Deluge BitTorrent Client" -s /usr/sbin/nologin -w no
+mkdir -p /home/deluge/.config/deluge     
+chown -R deluge:deluge /home/deluge/
+
+mkdir /Downloads
+chown deluge:deluge /Downloads
+
+# Configure the services
+sysrc -f /etc/rc.conf deluged_enable="YES"
+sysrc -f /etc/rc.conf deluged_user="deluge"
+sysrc -f /etc/rc.conf deluged_confdir="/home/deluge/.config/deluge"
+
+sysrc -f /etc/rc.conf deluge_web_enable="YES"
+sysrc -f /etc/rc.conf deluge_web_user="deluge"
+sysrc -f /etc/rc.conf deluge_web_confdir="/home/deluge/.config/deluge"
+
+echo "The initial password for the WebUI is: deluge" > /root/PLUGIN_INFO
 echo "To change, login and you will be prompted." >> /root/PLUGIN_INFO
+echo "The default download directory is: /Downloads" >> /root/PLUGIN_INFO
 
 # Start the services
 service deluged start
 service deluge_web start
+
