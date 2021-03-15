690124f21e002c74dfa7b22f601c54786fa95346
Jurgen Segaert <jsegaert@yahoo.com>
2021-03-14 17:55:35 -0700

    Add temporary workaround to downgrade libtorrent-rasterbar

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Temporary hack to downgrade libtorrent-rasterbar as per https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=254118
+pkg add -f https://github.com/jsegaert/binaries/releases/download/V0%2C1/py37-libtorrent-rasterbar-1.2.10.txz
+pkg add -f https://github.com/jsegaert/binaries/releases/download/V0%2C1/libtorrent-rasterbar-1.2.10.txz
+
+
 # Prepare the system
 pw useradd -n deluge -u 8675309 -m -c "Deluge BitTorrent Client" -s /usr/sbin/nologin -w no
 mkdir -p /home/deluge/.config/deluge     
